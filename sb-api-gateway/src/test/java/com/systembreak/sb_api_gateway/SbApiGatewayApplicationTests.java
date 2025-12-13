a71a86a073ffb0f68d0ff8863509ec8a2986bb1f
Anderson Blandon <anderson.blandon@hotmail.com>
2025-12-13 15:05:10 -0500

    feat: reinitialize sb-api-gateway with Spring Cloud Gateway base

---
+++ sb-api-gateway/src/test/java/com/systembreak/sb_api_gateway/SbApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systembreak.sb_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

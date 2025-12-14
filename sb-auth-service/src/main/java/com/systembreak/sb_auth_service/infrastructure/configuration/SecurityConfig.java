feb172380006e24f5db45ef8b325bc27d8995848
Santiago Villa <santiagovillapanesso1@gmail.com>
2025-12-14 16:08:18 -0500

    feat: Configuración de Docker Compose y entorno para depuración. Corregidos mapeos de puertos y AUTH_DB_HOST.

--- sb-auth-service/src/main/java/com/systembreak/sb_auth_service/infrastructure/configuration/SecurityConfig.java
+++ sb-auth-service/src/main/java/com/systembreak/sb_auth_service/infrastructure/configuration/SecurityConfig.java
@@ -47,8 +47,7 @@ public class SecurityConfig {
     @Bean
     // Spring inyecta automáticamente UserDetailsService (JpaUserDetailsService) y PasswordEncoder
     public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder);
         return authProvider;
     }
